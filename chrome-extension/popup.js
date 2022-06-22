89d30b7e91ca6017125a181881658e4a0953a5b9
Odell Blackmon III <blackmoniii.odell@gmail.com>
2022-06-21 21:17:08 -0400

    Made a basic extension. Working on API calls now

---
+++ chrome-extension/popup.js
@@ -0,0 +1,37 @@
+
+let searchBtn = document.getElementById("evc-button");
+
+console.log("Hello world!");
+// When the button is clicked, ask the API to search email for emails that match the website domain
+
+searchBtn.addEventListener('click', async () => {
+
+    getCurrentTab().then(currentTab => {
+
+        const domainName = stripUrl(currentTab.url);
+        // Construct an API request to Gmail API for messages with the same domain name in the past minute
+        changeHolderText(domainName);
+    });
+
+});
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+function changeHolderText(val){
+
+    let evcHolder = document.getElementById("evc-holder");
+    evcHolder.innerHTML = val;
+}
+
+// Strip url to just the domain
+function stripUrl(url){
+
+    const endIndex = url.indexOf(".com") + 4;
+
+    return url.slice(8, endIndex);
+}
