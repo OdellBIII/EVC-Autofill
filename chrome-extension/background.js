82ea21d8ca2d6476a63e64f00324687de0bf9ed3
OdellBIII <blackmoniii.odell@gmail.com>
2022-06-23 14:06:15 -0400

    This is the published version of the extension so that we can get an Application ID and use the chrome.identity API

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,5 +1,27 @@
 
 chrome.runtime.onInstalled.addListener(() => {
 
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.action.onClicked.addListener((currentTab) => {
+
+        const domainName = stripUrl(currentTab.url);
+        const verificationCodeEmail = 'noreply@' + domainName;
+
+        console.log("Hello world!");
+        chrome.identity.getAuthToken( {'interactive' : true}, (token) => {
+            console.log("Inside of callback");
+            console.log(token);
+        });
+    });
+
 });
+
+// Strip url to just the domain
+function stripUrl(url){
+
+    var result = url
+    // Trim off 'www.'
+    result = result.replace("www.","")
+    const endIndex = result.indexOf(".com") + 4;
+
+    return result.slice(8, endIndex);
+}
