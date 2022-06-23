ed99399af660cfe0adaa93518a1c56e082f6ae27
OdellBIII <blackmoniii.odell@gmail.com>
2022-06-22 22:36:51 -0400

    Ran into a wall with using a popup so now we are just using a service worker instead

---
+++ chrome-extension/background.js
@@ -0,0 +1,5 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,16 +1,21 @@
 
 let searchBtn = document.getElementById("evc-button");
+let emailForm = document.getElementById("emailForm");
+let evcHolder = document.getElementById("evc-holder");
 
-console.log("Hello world!");
 // When the button is clicked, ask the API to search email for emails that match the website domain
-
 searchBtn.addEventListener('click', async () => {
 
-    getCurrentTab().then(currentTab => {
+    chrome.tabs.getCurrent(currentTab => {
 
         const domainName = stripUrl(currentTab.url);
+        const verificationCodeEmail = 'noreply@' + domainName;
+        const email = emailForm.value;
         // Construct an API request to Gmail API for messages with the same domain name in the past minute
-        changeHolderText(domainName);
+        chrome.identity.getAuthToken( {'interactive' : true}, (token) => {
+
+            changeHolderText(token);
+        });
     });
 
 });
@@ -24,7 +29,6 @@ async function getCurrentTab() {
 
 function changeHolderText(val){
 
-    let evcHolder = document.getElementById("evc-holder");
     evcHolder.innerHTML = val;
 }
 
@@ -38,3 +42,26 @@ function stripUrl(url){
 
     return result.slice(8, endIndex);
 }
+/*
+function start() {
+  // 2. Initialize the JavaScript client library.
+  gapi.client.init({
+    'apiKey': 'AIzaSyBhyGyVaiDraTihyNOtL-tfyF0DPIMnzaA',
+    // Your API key will be automatically added to the Discovery Document URLs.
+    'discoveryDocs': ['https://gmail.googleapis.com/$discovery/rest?version=v1'],
+    // clientId and scope are optional if auth is not required.
+    'clientId': '122228461155-8rjqlq3168l5jpim8ls87cng9hesvsv9.apps.googleusercontent.com',
+    'scope': 'profile',
+  }).then(function() {
+    // 3. Initialize and make the API request.
+    return gapi.client.gmail.users.messages.list({
+      'userId': 'me',
+      'q': "from: "
+    });
+  }).then(function(response) {
+    console.log(response.result);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+};
+*/
